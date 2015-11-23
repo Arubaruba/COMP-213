7eed4e8478524abfa98b8a3902c5a4a8c1da0f64
Arubaruba <shadowstep7@gmail.com>
2015-11-23 13:05:57 +0200

    timeout instead of buttons

--- Exercise6.2/Splash.Designer.cs
+++ Exercise6.2/Splash.Designer.cs
@@ -29,47 +29,23 @@
         private void InitializeComponent()
         {
             this.autoshopNameLabel = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.closeButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // autoshopNameLabel
             // 
             this.autoshopNameLabel.AutoSize = true;
             this.autoshopNameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.25F);
-            this.autoshopNameLabel.Location = new System.Drawing.Point(13, 98);
+            this.autoshopNameLabel.Location = new System.Drawing.Point(12, 109);
             this.autoshopNameLabel.Name = "autoshopNameLabel";
             this.autoshopNameLabel.Size = new System.Drawing.Size(259, 29);
             this.autoshopNameLabel.TabIndex = 0;
             this.autoshopNameLabel.Text = "Pat’s auto repair shop";
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(113, 200);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(137, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "View Inventory";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // closeButton
-            // 
-            this.closeButton.Location = new System.Drawing.Point(33, 200);
-            this.closeButton.Name = "closeButton";
-            this.closeButton.Size = new System.Drawing.Size(61, 23);
-            this.closeButton.TabIndex = 2;
-            this.closeButton.Text = "Close";
-            this.closeButton.UseVisualStyleBackColor = true;
-            this.closeButton.Click += new System.EventHandler(this.closeButton_Click);
             // 
             // Splash
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
-            this.Controls.Add(this.closeButton);
-            this.Controls.Add(this.button1);
             this.Controls.Add(this.autoshopNameLabel);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Splash";
@@ -83,7 +59,5 @@
         #endregion
 
         private System.Windows.Forms.Label autoshopNameLabel;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button closeButton;
     }
 }
--- Exercise6.2/Splash.cs
+++ Exercise6.2/Splash.cs
@@ -5,7 +5,6 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Exercise6._2
@@ -15,18 +14,16 @@ namespace Exercise6._2
         public Splash()
         {
             InitializeComponent();
+
+            var timer = new Timer();
+            timer.Interval = 700;
+            timer.Tick += new EventHandler(onTimer);
+            timer.Start();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void onTimer(Object myObject, EventArgs myEventArgs)
         {
-            // We close this form allowing the 
             Close();
-        }
-
-        private void closeButton_Click(object sender, EventArgs e)
-        {
-            // We don't just close the form we exit entire app (preventing the AutoShop form from being created)
-            Application.Exit();
         }
     }
 }
