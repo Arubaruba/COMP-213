e82fd3959013f6c7e3b3aa8a242871fcffc0ee47
Andreas Stocker <shadowstep7@gmail.com>
2015-12-17 18:03:15 +0200

    add exercise 8.3 as part of hw 4

---
+++ HW4_Exercise8.3/SeatingPrice.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace HW4_Exercise8._3
+{
+    partial class SeatingPrice
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.seatingPositionComboBox = new System.Windows.Forms.ComboBox();
+            this.seatingPositionLabel = new System.Windows.Forms.Label();
+            this.priceLabel = new System.Windows.Forms.Label();
+            this.priceTextBox = new System.Windows.Forms.TextBox();
+            this.buyTicketButton = new System.Windows.Forms.Button();
+            this.priceButton = new System.Windows.Forms.Button();
+            this.printSummaryButton = new System.Windows.Forms.Button();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.SuspendLayout();
+            // 
+            // seatingPositionComboBox
+            // 
+            this.seatingPositionComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.seatingPositionComboBox.FormattingEnabled = true;
+            this.seatingPositionComboBox.Items.AddRange(new object[] {
+            "Orchestra",
+            "Mezzanine",
+            "General",
+            "Balcony"});
+            this.seatingPositionComboBox.Location = new System.Drawing.Point(211, 26);
+            this.seatingPositionComboBox.Name = "seatingPositionComboBox";
+            this.seatingPositionComboBox.Size = new System.Drawing.Size(121, 21);
+            this.seatingPositionComboBox.TabIndex = 0;
+            this.seatingPositionComboBox.SelectedIndexChanged += new System.EventHandler(this.seatingPositionComboBox_SelectedIndexChanged);
+            // 
+            // seatingPositionLabel
+            // 
+            this.seatingPositionLabel.AutoSize = true;
+            this.seatingPositionLabel.Location = new System.Drawing.Point(12, 26);
+            this.seatingPositionLabel.Name = "seatingPositionLabel";
+            this.seatingPositionLabel.Size = new System.Drawing.Size(83, 13);
+            this.seatingPositionLabel.TabIndex = 1;
+            this.seatingPositionLabel.Text = "Seating Position";
+            // 
+            // priceLabel
+            // 
+            this.priceLabel.AutoSize = true;
+            this.priceLabel.Location = new System.Drawing.Point(12, 69);
+            this.priceLabel.Name = "priceLabel";
+            this.priceLabel.Size = new System.Drawing.Size(31, 13);
+            this.priceLabel.TabIndex = 2;
+            this.priceLabel.Text = "Price";
+            // 
+            // priceTextBox
+            // 
+            this.priceTextBox.Location = new System.Drawing.Point(211, 66);
+            this.priceTextBox.Name = "priceTextBox";
+            this.priceTextBox.ReadOnly = true;
+            this.priceTextBox.Size = new System.Drawing.Size(121, 20);
+            this.priceTextBox.TabIndex = 3;
+            // 
+            // buyTicketButton
+            // 
+            this.buyTicketButton.Location = new System.Drawing.Point(257, 110);
+            this.buyTicketButton.Name = "buyTicketButton";
+            this.buyTicketButton.Size = new System.Drawing.Size(75, 23);
+            this.buyTicketButton.TabIndex = 4;
+            this.buyTicketButton.Text = "Buy Ticket";
+            this.toolTip1.SetToolTip(this.buyTicketButton, "Buy a ticket at the displayed price");
+            this.buyTicketButton.UseVisualStyleBackColor = true;
+            this.buyTicketButton.Click += new System.EventHandler(this.buyTicketButton_Click);
+            // 
+            // priceButton
+            // 
+            this.priceButton.Location = new System.Drawing.Point(12, 110);
+            this.priceButton.Name = "priceButton";
+            this.priceButton.Size = new System.Drawing.Size(75, 23);
+            this.priceButton.TabIndex = 5;
+            this.priceButton.Text = "Prices";
+            this.toolTip1.SetToolTip(this.priceButton, "Show the prices of different seats");
+            this.priceButton.UseVisualStyleBackColor = true;
+            this.priceButton.Click += new System.EventHandler(this.priceButton_Click);
+            // 
+            // printSummaryButton
+            // 
+            this.printSummaryButton.Location = new System.Drawing.Point(97, 110);
+            this.printSummaryButton.Name = "printSummaryButton";
+            this.printSummaryButton.Size = new System.Drawing.Size(93, 23);
+            this.printSummaryButton.TabIndex = 6;
+            this.printSummaryButton.Text = "Print Summary";
+            this.toolTip1.SetToolTip(this.printSummaryButton, "Show a summary of tickets bought");
+            this.printSummaryButton.UseVisualStyleBackColor = true;
+            this.printSummaryButton.Click += new System.EventHandler(this.printSummaryButton_Click);
+            // 
+            // SeatingPrice
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(344, 145);
+            this.Controls.Add(this.printSummaryButton);
+            this.Controls.Add(this.priceButton);
+            this.Controls.Add(this.buyTicketButton);
+            this.Controls.Add(this.priceTextBox);
+            this.Controls.Add(this.priceLabel);
+            this.Controls.Add(this.seatingPositionLabel);
+            this.Controls.Add(this.seatingPositionComboBox);
+            this.Name = "SeatingPrice";
+            this.Text = "Seating Price";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox seatingPositionComboBox;
+        private System.Windows.Forms.Label seatingPositionLabel;
+        private System.Windows.Forms.Label priceLabel;
+        private System.Windows.Forms.TextBox priceTextBox;
+        private System.Windows.Forms.Button buyTicketButton;
+        private System.Windows.Forms.Button priceButton;
+        private System.Windows.Forms.Button printSummaryButton;
+        private System.Windows.Forms.ToolTip toolTip1;
+    }
+}
+
---
+++ HW4_Exercise8.3/SeatingPrice.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Printing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HW4_Exercise8._3
+{
+    public partial class SeatingPrice : Form
+    {
+        int orchestraTotal = 0;
+        int mezzanineTotal = 0;
+        int generalTotal = 0;
+        int balconyTotal = 0;
+
+        public SeatingPrice()
+        {
+            InitializeComponent();
+
+            // Make sure the seat select does not start out empty
+            seatingPositionComboBox.Text = seatingPositionComboBox.Items[0].ToString();
+
+            // update the displayed price to reflect the initial value
+            seatingPositionComboBox_SelectedIndexChanged(null, null);
+        }
+
+        /// <summary>
+        /// When the prices button is clicked this function will create a popup
+        /// displaying the prices of all seating positions
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void priceButton_Click(object sender, EventArgs e)
+        {
+            string prices = "Orchestra 40.00\nMezzanine 27.50\nGeneral 15.00\nBalcony 10.00";
+            MessageBox.Show(prices, "Prices");
+        }
+
+        private void printSummaryButton_Click(object sender, EventArgs e)
+        {
+            // Boilerplate so we can show the print preview dialog
+            var printDocument = new PrintDocument();
+            printDocument.PrintPage += new PrintPageEventHandler(onPrint);
+
+            var printDialog = new PrintPreviewDialog();
+            printDialog.Document = printDocument;
+            printDialog.ShowDialog();
+        }
+
+        /// <summary>
+        /// Draws a string when printSummaryButton is clicked
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void onPrint(object sender, PrintPageEventArgs e)
+        {
+            string summary = "Summary of Tickets bought:\n\nOrchestra " + orchestraTotal +
+                "\nMezzanine " + mezzanineTotal +
+                "\nGeneral " + generalTotal +
+                "\nBalcony " + balconyTotal;
+
+            e.Graphics.DrawString(summary, new Font("Arial", 20), Brushes.Black, 0, 0);
+        }
+
+        /// <summary>
+        /// Update the summary to reflect the bought tickets
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buyTicketButton_Click(object sender, EventArgs e)
+        {
+            switch (seatingPositionComboBox.Text)
+            {
+                case "Orchestra": orchestraTotal++; break;
+                case "Mezzanine": mezzanineTotal++; break;
+                case "General": generalTotal++; break;
+                case "Balcony": balconyTotal++; break;
+            }
+
+            MessageBox.Show("Ticket for " + seatingPositionComboBox.Text + " seat purchased", "Success");
+        }
+
+        private void seatingPositionComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            decimal price = 0;
+
+            switch (seatingPositionComboBox.Text)
+            {
+                case "Orchestra": price = 40; break;
+                case "Mezzanine": price = 27.50M; break;
+                case "General": price = 15; break;
+                case "Balcony": price = 10; break;
+            }
+
+            priceTextBox.Text = price.ToString("C");
+        }
+    }
+}
